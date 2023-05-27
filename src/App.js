4e07598d3b3f354be0b9dc32725d19947b37ba55
Tejas <tejaskothari03@gmail.com>
2023-05-28 03:12:55 +0530

    Functional header with theme icon

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
 import React, { useState } from 'react';
 import './App.css';
-import sunImage from './sun.jpg';
-import moonImage from './moon.jpg';
+import sunIcon from './sun.svg';
+import moonIcon from './moon.svg';
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
-  const handleToggle = () => {
-    setIsDarkMode(prevMode => !prevMode);
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
   };
 
   return (
     <div className={`App ${isDarkMode ? 'dark' : 'light'}`}>
-      <div className="slider-container">
-        <input type="checkbox" className="slider" id="slider" onChange={handleToggle} />
-        <label htmlFor="slider" className="slider-label">
-          <div className={`slider-button ${isDarkMode ? 'night' : 'day'}`}>
-            <div className="slider-icon">
-              <img src={isDarkMode ? moonImage : sunImage} alt="Icon" className="icon" />
-            </div>
-          </div>
-        </label>
-      </div>
-      <h1>Welcome to My Website</h1>
-      <p>This is a sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <header className="header">
+        <div className="title">Tejas Kothari</div>
+        <button className="theme-toggle" onClick={toggleTheme}>
+          <img src={isDarkMode ? moonIcon : sunIcon} alt="Theme toggle" />
+        </button>
+      </header>
+      {/* Rest of your application content */}
     </div>
   );
 }
