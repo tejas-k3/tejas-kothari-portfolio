8f1737fa3318f8aef48682b41c769ac7972cf8ea
Tejas <tejaskothari03@gmail.com>
2023-05-28 01:42:46 +0530

    new portfolio init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import sunImage from './sun.jpg';
+import moonImage from './moon.jpg';
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const handleToggle = () => {
+    setIsDarkMode(prevMode => !prevMode);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${isDarkMode ? 'dark' : 'light'}`}>
+      <div className="slider-container">
+        <input type="checkbox" className="slider" id="slider" onChange={handleToggle} />
+        <label htmlFor="slider" className="slider-label">
+          <div className={`slider-button ${isDarkMode ? 'night' : 'day'}`}>
+            <div className="slider-icon">
+              <img src={isDarkMode ? moonImage : sunImage} alt="Icon" className="icon" />
+            </div>
+          </div>
+        </label>
+      </div>
+      <h1>Welcome to My Website</h1>
+      <p>This is a sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
   );
 }
